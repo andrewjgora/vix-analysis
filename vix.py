8f0d19f483cd35ab00a862d5c945c8e0223abafe
Andrew Gora <agora@umass.edu>
2015-01-31 14:47:28 -0500

    prints lows excel sheet

--- vix.py
+++ vix.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import Quandl as q #imports Quandl and assigns 'q' as its shortcut so we can just type q.get() etc
 import os
+from openpyxl import Workbook
 
 vix = q.get("YAHOO/INDEX_VIX")
 #vixmonthly = q.get("YAHOO/INDEX_VIX", collapse="monthly")
@@ -16,51 +17,27 @@ vix = q.get("YAHOO/INDEX_VIX")
 #leap years: 1992, 1996, 2000, 2004, 2008, 2012
 
 monthlim = {1: 31 , 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
-indextomonth = {1: 'January',
-		   2: 'February',
-		   3: 'March',
-		   4: 'April',
-		   5: 'May',
-		   6: 'June',
-		   7: 'July',
-		   8: 'August',
-		   9: 'September',
-		   10: 'October',
-		   11: 'November',
-		   12: 'December'}
+xltomonth = {2: 'January',
+		  	    3: 'February',
+		   		4: 'March',
+		   		5: 'April',
+		   		6: 'May',
+		   		7: 'June',
+		   		8: 'July',
+		   		9: 'August',
+		   		10: 'September',
+		   		11: 'October',
+		   		12: 'November',
+		   		13: 'December'}
 
 def printextrema(startyear, endyear):
 
-	# for curyear in range(startyear, endyear + 1):
-	# 	for curmonth in range(0, 12):
-	# 		for curday in range(0, monthlim[curmonth + 1]): #find lowest low & highest high of each month
-	# 			try:
-	# 				lowval = 999
-	# 				highval = 0
-	# 				if curday == 0:
-	# 					lowval = vix.loc['%d-%d-%d' % (curyear, curmonth + 1, curday + 1),'Low']
-	# 					highval = vix.loc['%d-%d-%d' % (curyear, curmonth + 1, curday + 1),'High']
-	# 				else:
-	# 					nextlow = vix.loc['%d-%d-%d' % (curyear, curmonth + 1, curday + 1),'Low']
-	# 					nexthigh = vix.loc['%d-%d-%d' % (curyear, curmonth + 1, curday + 1),'High']				
-	# 					if nextlow < lowval:
-	# 						lowdate = '%d-%d-%d' % (curyear, curmonth + 1, curday + 1)
-	# 						lowval = nextlow
-	# 					if nexthigh > highval:
-	# 						highdate = '%d-%d-%d' % (curyear, curmonth + 1, curday + 1)
-	# 						highval = nexthigh
-	# 			except KeyError:
-	# 				hi = 1
-	# 		print('Lowest Low for %s: %d' % (lowdate, lowval))
-	# 		print('Highest High for %s: %d' % (highdate, highval))
-	# 		print()
-
-
-	lowdate = ''
-	highdate = ''
 	curlow = np.nan
 	curhigh = np.nan
+
 	for year in range(startyear, endyear):
+		#add new row to lows spreadsheet
+		#add new row to highs spreadsheet
 		for month in range(12):
 			for day in range(monthlim[month + 1]):
 				if year == startyear:
@@ -71,7 +48,7 @@ def printextrema(startyear, endyear):
 				lastopen = vix.index.asof(curdatestr)
 				curdate = pd.to_datetime(curdatestr)
 
-				if curdate.is_month_start == True:
+				if curdate.is_month_start:
 					curlow = 999
 					curhigh = 0
 				nextlow = vix.loc[lastopen, 'Low']
@@ -86,17 +63,54 @@ def printextrema(startyear, endyear):
 					break
 			print('_______________________________________')
 			print()
-			print('Extrema for %s, %d:' % (indextomonth[month + 1], year))
+			print('Extrema for %s, %d:' % (xltomonth[month + 1], year))
 			print()
-			print('Lowest Low: $%d on %s %d' % (curlow, indextomonth[lowdate.month], lowdate.day))
-			print('Highest High: $%d on %s %d' %  (curhigh, indextomonth[highdate.month], highdate.day))
-			print(month)
+			print('Lowest Low: $%d on %s %d' % (curlow, xltomonth[lowdate.month], lowdate.day))
+			print('Highest High: $%d on %s %d' %  (curhigh, xltomonth[highdate.month], highdate.day))
 			print('_______________________________________')
-			#date_range
+			#
+	
 
 
-		
-printextrema(1990, 2015)
+def printexcelsheet(startyear, endyear, sheetname):
 
-#s.to_frame(name='column_name').to_excel('xlfile.xlsx', sheet_name='s')
+	curlow = np.nan
+	xlbook = Workbook()
+	xsheet = xlbook.active
+	xsheet.title = sheetname
+	# compare = {'High': }
 
+	# def high(val):
+	# 	if val > curx:
+	# 		xdate = curdate
+	# 		curx =
+
+	# create the cells for January-December
+	for i in range(2, 14):
+		xsheet.cell(row = 1, column = i).value = xltomonth[i]
+
+	for year in range(startyear, endyear):
+
+		xlrow = year - startyear + 2
+		xsheet.cell(row = xlrow, column = 1).value = year
+		for month in range(12):
+			for day in range(monthlim[month + 1]):
+				if year == startyear:
+					day += 1
+					curx = 999
+				curdatestr = '%d-%d-%d' % (year, month + 1, day + 1)
+				lastopen = vix.index.asof(curdatestr)
+				curdate = pd.to_datetime(curdatestr)
+				if curdate.is_month_start:
+					curx = 999
+				nextx = vix.loc[lastopen, 'Low']
+				if nextx < curx:
+					xdate = curdate
+					curx = nextx
+				if curdate.is_month_end: #we know we found the lowest now, so break
+					break
+			xsheet.cell(row = xlrow, column = month+2).value = curx #add value to excel cell
+
+	xlbook.save('%s.xlsx' % sheetname)
+
+printexcelsheet(1990, 2015, 'All_Lows')
